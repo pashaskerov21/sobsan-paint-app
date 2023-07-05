e1a67dfa58a4aa7469f8d0c70991571d30661ccc
pasha <askerovpasha21@gmail.com>
2023-07-05 10:04:50 +0400

    payment&delivery and warranty page completed

--- src/pages/PaymentDelivery.jsx
+++ src/pages/PaymentDelivery.jsx
@@ -1,10 +1,41 @@
 import React from 'react'
 import PrimarySection from '../components/sections/PrimarySection'
+import image_1 from '../image/about/payment-delivery.jpg'
+import TextTranslate from '../translate/TextTranslate'
 
 function PaymentDelivery() {
   return (
     <PrimarySection className='payment-delivery' path='payment-and-delivery' rootLink='Ödəniş və Çatdırılma' sectionTitle='Ödəniş və Çatdırılma'>
-
+      <div className="text-container">
+        <div className="row">
+          <div className="col-12 col-lg-6">
+            <div className="text">
+              <span className="title"><TextTranslate text='Ödəniş üsulları :'/></span>
+              <ul>
+                <li><TextTranslate text='Təhvil alarkən NAĞD şəklində ödəyə bilərsiniz.'/></li>
+                <li><TextTranslate text='"Online" olaraq bank  vasitəsilə;'/></li>
+                <li><TextTranslate text='Təhvil nöqtəsində POS terminal vasitəsilə;'/></li>
+              </ul>
+              <span className="title"><TextTranslate text='Çatdırılma Qaydaları :'/></span>
+              <ul>
+                <li><TextTranslate text='Çatdırılma bütün ölkə daxilində həyata keçirilir.'/></li>
+                <li><TextTranslate text='Sifariş olunan məhsulun və ya məhsulların ümumi dəyəri 200 AZN-dən çox olarsa, çatdırılma  pulsuz olunur. Sifariş olunan məhsul və ya məhsulların ümumi dəyəri 200 AZN-dən aşağı olduqda 10 AZN ödəniş tələb olunur.'/></li>
+                <li><TextTranslate text='Bakı şəhəri üzrə sifariş olunan məhsullar 1(bir )iş günü müddətində müştərilərə çatdırılır.'/></li>
+                <li><TextTranslate text='Regionlara çatdırılma 5(beş) iş günü ərzində müştərilərə çatdırılır.'/></li>
+                <li><TextTranslate text='Sifarişlər fərdi yaşayış evləri istisna olmaqla, binaların girişinə qədər çatdırılır. Məhsulun  boşaldılması və ya mərtəbəyə qaldırılması çatdırılma xidmətinə daxil deyildir.'/></li>
+                <li><TextTranslate text='Təhvil zamanı məhsullar fiziki qaydada həm alıcı tərəfindən yoxlanılaraq təhvil alınır, həm də əməkdaş tərəfindən yoxlanaraq təhvil verilir. Məhsulu aldığınız zaman xarici görünüşünə diqqət yetirin və heç bir texniki zədələnmə olmadığından əmin olun. Əməkdaşdan qaimə tələb edin və imzalayın.'/></li>
+                <li><TextTranslate text='İstirahət və bayram günlərində sifarişlərdə gecikmələr ola bilər.'/></li>
+                <li><TextTranslate text='Bəzi məhsullar sifariş əsasında hazırlandığı üçün ( anbarda hazır şəkildə stokda mövcud olmadığına görə) hazırlanıb çatdırılmasında gecikmə ola bilər.'/></li>
+              </ul>
+            </div>
+          </div>
+          <div className="col-12 col-lg-6">
+            <div className="image">
+              <img src={image_1} alt="about" />
+            </div>
+          </div>
+        </div>
+      </div>
     </PrimarySection>
   )
 }
--- src/pages/WarrantyConditions.jsx
+++ src/pages/WarrantyConditions.jsx
@@ -1,10 +1,39 @@
 import React from 'react'
 import PrimarySection from '../components/sections/PrimarySection'
+import TextTranslate from '../translate/TextTranslate'
+import image_1 from '../image/about/warranty-condition.jpg'
+
 
 function WarrantyConditions() {
   return (
     <PrimarySection className='warranty-conditions' path='warranty-conditions' rootLink='Zəmanət və Geri Qaytarma Siyasəti' sectionTitle='Zəmanət və Geri Qaytarma Siyasəti'>
-
+      <div className="text-container">
+        <div className="row">
+          <div className="col-12 col-lg-6">
+            <div className="text">
+              <span className="title"><TextTranslate text='Zəmanət :' /></span>
+              <ul>
+                <li><TextTranslate text='Məhsulların keyfiyyət və orijinallığına görə şirkət tam şəkildə məsuliyyət daşıyır.' /></li>
+                <li><TextTranslate text='Boya məhsulları və quru inşaat qarışıqlarının istifadə və saxlama müddəti qablaşmanın üzərində göstərilmişdir.' /></li>
+                <li><TextTranslate text='Məhsulların istifadə və saxlanma təlimatına əməl olunmadığı təqdirdə, istehsalçı şirkət ortaya çıxan qüsur və nəticələrə görə məsuliyyət daşımır !' /></li>
+                <li><TextTranslate text='"Online" ödənişlərdə müştərilərin kart təhlükəsizliyi tam yerinə yetirilir və kart sahibinin məxfi  kart məlumatları heç bir şəkildə saxlanılmır.' /></li>
+              </ul>
+              <span className="title"><TextTranslate text='Geri qaytarma :' /></span>
+              <ul>
+                <li><TextTranslate text='Məhsul təhvil alındığı anda: Əgər sifariş edilən məhsul müştərini əsaslı səbəblə qane etmirsə, yəni sifarişə uyğun olmayan məhsul göndərilibsə, məhsulun qablaşmasında fiziki qüsur varsa,(açılmış,zədələnmiş və ya məhsul zərər görmüşsə) qaytarıla bilər. Bu halda  geri qaytarma aktı tərtib edilərək, tərəflər təsdiq etdikdən sonra məhsul əməkdaş vasitəsilə geri qaytarılaraq anbara təhvil verilir.' /></li>
+                <li><TextTranslate text='Geri qaytarılma nağd alış-verişdə həftə içi 5 gün 09:00- 17:00 arası, “online” ödəmələr bankın müəyyən etdiyi zaman dilimində hesaba qaytarılacaq.' /></li>
+                <li><TextTranslate text='Müştəri “online” ödəmə etmişsə, geri qaytarma aktı əsasında əlaqədar banka ödəmənin geri qaytarılması üçün müraciət edilir. Bankın daxili proseduruna uyğun olaraq pul müştərinin hesabına müəyyən olunmuş müddətdə geri ödənilir.' /></li>
+                <li><TextTranslate text='Müştəri tərəfindən geri qaytarılacaq olan məhsullar Bakı şəhəri və regionlarda  satış menecerləri vasitəsilə  müştəri ilə  əlaqə saxlanılaraq  uyğun ünvanda təhvil alınır.' /></li>
+              </ul>
+            </div>
+          </div>
+          <div className="col-12 col-lg-6">
+            <div className="image">
+              <img src={image_1} alt="about" />
+            </div>
+          </div>
+        </div>
+      </div>
     </PrimarySection>
   )
 }
